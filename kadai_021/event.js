788f6ace24709067ca67750eff9541c3e2debc69
yami8383 <yami8000030000@gmail.com>
2025-06-28 14:29:24 +0900

    Add files via upload

---
+++ kadai_021/event.js
@@ -0,0 +1,9 @@
+// output-btnというidを持つHTML要素を取得し、定数に代入する
+const btn = document.getElementById('btn');
+const text =document.getElementById('text');
+// HTML要素がクリックされたときにイベント処理を実行する
+btn.addEventListener('click', () => {
+   setTimeout(() =>{
+     text.textContent = 'ボタンをクリックしました';
+   },2000);
+});
